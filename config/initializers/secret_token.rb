ddde806a6c058ebcc1a2bb5c67a09ccd63c7d5d1
Gregory Sheppard <gregory.sheppard@live.com>
2014-03-07 13:06:48 -0500

    Initial commit. Added Veekun database to the configuration. Scraped schema from it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokedb::Application.config.secret_key_base = '1ed9fcc03163e6731e8061154c369107305efe6001725318fc32af943934346bc6f9cb1a147900e1e7e979f6562dbaedd412a3857e4353c95d8a41a9d1d14600'
