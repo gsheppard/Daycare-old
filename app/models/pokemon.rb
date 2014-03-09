311b7d76053079f586b2236bd8be446fc2873439
Gregory Sheppard <gregory.sheppard@live.com>
2014-03-09 18:12:47 -0400

    Added Pokemon<->Base Stat relationships and Nature<->Stat relationships

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -9,4 +9,7 @@ class Pokemon < ActiveRecord::Base
   has_many :pokemon_abilities
   has_many :abilities, through: :pokemon_abilities
 
+  has_many :pokemon_stats
+  has_many :stats, through: :pokemon_stats
+
 end
--- app/models/pokemon_stat.rb
+++ app/models/pokemon_stat.rb
@@ -1,2 +1,6 @@
 class PokemonStat < ActiveRecord::Base
+
+  belongs_to :pokemons, class_name: "Pokemon", foreign_key: "pokemon_id"
+  belongs_to :stats, class_name: "Stat", foreign_key: "stat_id"
+
 end
